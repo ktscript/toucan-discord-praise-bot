fe0757542875d51117a8d0b0abfdd9c7bb0e3683
lazaralex98 <alex2012ro@gmail.com>
2022-01-09 17:31:07 +0200

    praise method in Token contract

--- contracts/ToucanPraiseToken.sol
+++ contracts/ToucanPraiseToken.sol
@@ -8,4 +8,30 @@ contract ToucanPraiseToken is ERC20 {
     constructor() ERC20("ToucanPraiseToken", "TPT") {
         _mint(msg.sender, _INITIAL_SUPPLY);
     }
+
+    /**
+    * @param _to the address to praise
+    * @return true upon success
+    *
+    * Mints & sends praiseTokens to an address
+    */
+    function praise(address _to) public returns (bool) {
+        /**
+        * It's important to think of your praiseToken balance as your reputation.
+        */
+        uint256 reputation = balanceOf(msg.sender);
+
+        /**
+        * This makes the praise have a worth based on who praised.
+        * If someone more reputable praises, then the praise will be worth more.
+        */
+        uint256 praiseWorth = reputation / 10;
+
+        /**
+        * We make sure the person that praises doesn't lose any reputation.
+        * Minting more, also lowers the value of each individual token, adding an element similar to 'decaying'
+        */
+        _mint(_to, praiseWorth);
+        return true;
+    }
 }
