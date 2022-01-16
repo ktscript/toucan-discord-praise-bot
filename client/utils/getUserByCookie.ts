a1bea24d9d63e97bc54b6b83013f183ace639903
lazaralex98 <alex2012ro@gmail.com>
2022-01-17 01:39:15 +0200

    let's test my supabase.auth.api.getUserByCookie() replacement live

---
+++ client/utils/getUserByCookie.ts
@@ -0,0 +1,43 @@
+import { ApiError, User } from "@supabase/supabase-js";
+
+interface ifcUserByCookie {
+  user: User | null;
+  error: string | null;
+}
+
+// this function replaces the broken supabase.auth.api.getUserByCookie()
+const getUserByCookie = async (req: any): Promise<ifcUserByCookie> => {
+  let token = req.cookies["sb:token"];
+  if (!token) {
+    return {
+      user: null,
+      error: "There is no supabase token in request cookies",
+    };
+  }
+  let authRequestResult = await fetch(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/user`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        APIKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+      },
+    }
+  );
+
+  let result = await authRequestResult.json();
+  console.log("Supabase auth result", result);
+  if (authRequestResult.status != 200) {
+    return {
+      user: null,
+      error: `Supabase auth returned ${authRequestResult.status}. See logs for details`,
+    };
+  } else if (result.aud === "authenticated") {
+    return {
+      user: result,
+      error: null,
+    };
+  }
+  return { user: null, error: "Unexpected error" };
+};
+
+export default getUserByCookie;
