6731bb36f65114756a4781ec56319e234822f438
lazaralex98 <alex2012ro@gmail.com>
2022-01-18 22:24:03 +0200

    cleanup help command

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -6,6 +6,7 @@ import { handlePraiseCommand } from "./commandHandlers/handlePraiseCommand";
 import discordToWalletConnection from "./utils/ifcDiscordtoWalletConnection";
 import fetchWalletConnection from "./utils/fetchWalletConnection";
 import handleConnectCommand from "./commandHandlers/handleConnectCommand";
+import handleHelpCommand from "./commandHandlers/handleHelpCommand";
 require("dotenv").config();
 
 const PREFIX = "!";
@@ -27,18 +28,7 @@ discord.on("messageCreate", (msg: Message) => {
   if (!parsed.success) return;
 
   if (parsed.command === "help") {
-    console.log(`${msg.author} needs help!`);
-    msg.react("🌳");
-    msg.reply(
-      `Hey there, I'm the Praise Bot. Here's a list of the commands that you can use: ` +
-        `\n\n- "!connect" to connect your discord & wallet to this bot so you can praise & be praised` +
-        `\n\n- "!profile" will let you know if your discord or wallet are connected to the bot` +
-        `\n\n- "!praise @john.appleseed for [insert reason]" to praise someone with the reason for the praise. ` +
-        `It's very important to tag them before stating the reason. Stating a reason is optional, ` +
-        `but if you do, you have to mark it by using the "for" keyword before stating the reason.` +
-        `\n\n Those are all the commands. It's important to note that the amount of TPT the person you praise ` +
-        `receives is calculated based on how much TPT you have yourself. In this way, TPT acts like reputation.`
-    );
+    handleHelpCommand(msg);
   }
 
   if (parsed.command === "connect") {
