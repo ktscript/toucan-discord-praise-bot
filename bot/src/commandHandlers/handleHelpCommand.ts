eda89d2f0054e35f165add96d0aef5f06d0c23cb
lazaralex98 <alex2012ro@gmail.com>
2022-01-22 22:44:19 +0200

    better help message

--- bot/src/commandHandlers/handleHelpCommand.ts
+++ bot/src/commandHandlers/handleHelpCommand.ts
@@ -14,7 +14,9 @@ const handleHelpCommand = (msg: Message) => {
       `It's very important to tag them before stating the reason. Stating a reason is optional, ` +
       `but if you do, you have to mark it by using the "for" keyword before stating the reason.` +
       `\n\n Those are all the commands. It's important to note that the amount of TPT the person you praise ` +
-      `receives is calculated based on how much TPT you have yourself. In this way, TPT acts like reputation.`
+      `receives is calculated based on how much TPT you have yourself. In this way, TPT acts like reputation.` +
+      `\n\n One more important thing, this is the token's address: 0x5C3D88A68d3AD54CfEB87d56Cc64421E3EEEC347.` +
+      `\n\n You will use this address to import the token into your wallet.`
   );
 };
 
