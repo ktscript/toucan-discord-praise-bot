6f57bee6ba3229d5762e0ee910d275c5f2a705a3
lazaralex98 <alex2012ro@gmail.com>
2022-02-19 20:10:55 +0200

    api error handling

--- client/pages/api/slackOAuth.ts
+++ client/pages/api/slackOAuth.ts
@@ -15,37 +15,38 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // TODO this should be the on inserting an ifcInstallation into db
-  const code = req.query["code"];
+  try {
+    // TODO this should be the on inserting an ifcInstallation into db
+    const code = req.query["code"];
+    if (!code || code == "") throw new Error("No code provided");
 
-  const slack: Slack = new Slack({
-    token: process.env.SLACK_BOT_TOKEN, // TODO this will need to be taken from dB, but how does this know which token to take
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
-  });
-  const access = await slack.client.oauth.v2.access({
-    code: String(code),
-    client_id: process.env.SLACK_CLIENT_ID || "",
-    client_secret: process.env.SLACK_CLIENT_SECRET || "",
-  });
+    const slack: Slack = new Slack({
+      token: process.env.SLACK_BOT_TOKEN, // TODO this will need to be taken from dB, but how does this know which token to take
+      signingSecret: process.env.SLACK_SIGNING_SECRET,
+      socketMode: true,
+      appToken: process.env.SLACK_APP_TOKEN,
+    });
+    const access = await slack.client.oauth.v2.access({
+      code: String(code),
+      client_id: process.env.SLACK_CLIENT_ID || "",
+      client_secret: process.env.SLACK_CLIENT_SECRET || "",
+    });
 
-  console.log("access", access);
+    const { data, error } = await supabase.from("slack_installations").insert([
+      {
+        enterprise_id: access.enterprise?.id,
+        team_id: access.team?.id,
+        bot_token: access.access_token,
+        bot_id: access.app_id, // TODO could be wrong. example: B5910
+        bot_user_id: access.bot_user_id,
+      },
+    ]);
+    if (error) throw new Error("Error when inserting data in dB");
 
-  const { data, error } = await supabase.from("slack_installations").insert([
-    {
-      enterprise_id: access.enterprise?.id,
-      team_id: access.team?.id,
-      bot_token: access.access_token,
-      bot_id: access.app_id, // TODO could be wrong. example: B5910
-      bot_user_id: access.bot_user_id,
-    },
-  ]);
-  if (error || !req.query["code"]) {
-    res.status(500).json({ message: "Some error happened", error: error });
-  } else {
     res
       .status(200)
       .json({ status: "OK", message: "The bot has been installed." });
+  } catch (error: any) {
+    res.status(500).json({ status: "NOT OK", message: error.message });
   }
 }
